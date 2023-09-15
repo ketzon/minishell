769bb9004883f275327729a78b99f7e59fcc8a0b
Parallels <francois.gnz@gmail.com>
2023-09-15 13:21:59 +0200

    cleaning branch

--- srcs/utils.c
+++ srcs/utils.c
@@ -5,24 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/04 17:44:56 by fgonzale          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/09/13 16:43:30 by fgonzale         ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2023/09/07 17:17:52 by fbesson          ###   ########.fr       */
->>>>>>> master
+/*   Created: 2023/09/15 13:21:12 by fgonzale          #+#    #+#             */
+/*   Updated: 2023/09/15 13:21:20 by fgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-=======
-/*   Updated: 2023/09/05 17:03:30 by fbesson          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "minishell.h"
->>>>>>> 5684930f30fb02b89ff62a932172219cabebd101
 
 /* SAUTE LES ESPACES DANS UNE STRING ET RENVOIE DE COMBIEN IL A AVANCE */
 int ft_skip_white_spaces(char *str)
@@ -63,7 +51,3 @@ t_lexer *new_node(char *input, t_token token)
     return (new);
 }
 
-int	whitespace(char c)
-{
-	return (c == ' ' || c == " " || c == '\t');
-}
