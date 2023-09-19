4c568b9eb20c6d0a59a3abd268037a810d7db35f
Parallels <francois.gnz@gmail.com>
2023-09-19 14:11:41 +0200

    lexer updated

--- srcs/variables.c
+++ srcs/variables.c
@@ -6,13 +6,37 @@
 /*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 19:49:54 by fgonzale          #+#    #+#             */
-/*   Updated: 2023/09/19 13:58:38 by fgonzale         ###   ########.fr       */
+/*   Updated: 2023/09/19 14:10:46 by fgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void is_there_variable (t_lexer *node)
+static bool  check_invalid_consecutive_tokens(t_lexer *node)
+{
+    if (node->token == PIPE && node->previous && node->previous->token == PIPE)
+        return (true);
+    if (node->token > PIPE && node->previous && node->previous->token > PIPE)
+        return (true);
+    if (node->token >= PIPE && node->next == NULL)
+        return (true);
+    return (false);
+}
+
+static bool check_invalid_tokens(t_lexer *node)
+{
+    if (check_invalid_consecutive_tokens(node) == true)
+    {
+        if (node->next == NULL && node->token > PIPE)
+            printf("syntax error near unexpected token 'newline'\n"); // CHANGER PARCE QUE C'EST PAS 100% WORKING
+        else
+            printf("syntax error near unexpected token '%s'\n", node->word);
+        return (true);
+    }
+    return (false);
+}
+
+static void flag_variables (t_lexer *node)
 {
     int i;
 
